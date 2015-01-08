acbe2a5957ea6b2e8044f83096997a6292be8f06
Desarrollo AutoTaxi <desarrolloautotaxi@MacBook-Air-de-Desarrollo-3.local>
2015-01-08 10:46:16 -0500

    Initial Commit

---
+++ PrimaRiesgoSimple/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PrimaRiesgoSimple
+//
+//  Created by Desarrollo AutoTaxi on 1/8/15.
+//  Copyright (c) 2015 Master Apps Movil - UNIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ PrimaRiesgoSimpleTests/PrimaRiesgoSimpleTests.m
@@ -0,0 +1,40 @@
+//
+//  PrimaRiesgoSimpleTests.m
+//  PrimaRiesgoSimpleTests
+//
+//  Created by Desarrollo AutoTaxi on 1/8/15.
+//  Copyright (c) 2015 Master Apps Movil - UNIR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface PrimaRiesgoSimpleTests : XCTestCase
+
+@end
+
+@implementation PrimaRiesgoSimpleTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
